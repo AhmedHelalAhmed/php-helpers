026beb6b18d4cfdb1355de1e9e1b495c4eca890a
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-07-25 15:08:24 +0200

    Add filterHTML

--- helpers.php
+++ helpers.php
@@ -2,7 +2,16 @@
 
 // check if pattern exists in statemant
 // word in statement
-function isPatternExists($pattern, $statement)
-{
-    return strpos($statement, $pattern)!==false;
+if (!function_exists('isPatternExists')) {
+    function isPatternExists($pattern, $statement)
+    {
+        return strpos($statement, $pattern)!==false;
+    }
+}
+
+if (!function_exists('filterHTML')) {
+    function filterHTML($html)
+    {
+        return htmlentities($html);
+    }
 }
--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 
 require 'helpers.php';
 
-
 if (isPatternExists("hello", "hello world!")) {
     echo 'exists';
 } else {
     echo 'not exists';
 }
+
+echo "<br/>";
+echo "===============================";
+echo "<br/>";
+
+echo filterHTML('<b>test</b>');
+
+echo "<br/>";
+echo "===============================";
+echo "<br/>";
